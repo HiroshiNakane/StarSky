5e31fab5934059845cce63bf6c90b7e127408150
中根寛志 <tsmus3105@gmail.com>
2019-05-12 17:04:01 +0900

    間違えて3Dで作ってたので2Dで作り直し

---
+++ StarSky/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    Rigidbody rb;
+    float moveSpeed = 4.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, moveSpeed);
+    }
+
+    void Update()
+    {
+        
+    }
+}
