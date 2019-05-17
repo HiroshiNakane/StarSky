0b4cdee028bf48e5d556db518eda8dff19770a4e
中根寛志 <tsmus3105@gmail.com>
2019-05-17 14:41:23 +0900

    シーン追加とクリック(タップ)した時の座標取得

--- StarSky/Assets/Scripts/PlayerMove.cs
+++ StarSky/Assets/Scripts/PlayerMove.cs
@@ -6,11 +6,20 @@ public class PlayerMove : MonoBehaviour
 {
 
     Rigidbody rb;
+    // 移動速度
     float moveSpeed = 4.0f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        // プレイヤーの座標の取得
+        Vector3 playerPos = GameObject.Find("Player").transform.position;
+
+        // タップした座標の取得
+        Vector3 tapPosScreen = Input.mousePosition;
+        tapPosScreen.z = 10;
+        Vector3 tapPosWorld = Camera.main.ScreenToWorldPoint(tapPosScreen);
     }
 
     void FixedUpdate()
