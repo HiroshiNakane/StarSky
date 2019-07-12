03f7f5ea1c2e7e2ab36403195692d24fbb3d1ee4
中根寛志 <tsmus3105@gmail.com>
2019-07-12 11:07:16 +0900

    MainSceneのテキスト変更

--- StarSky/Assets/Scripts/StartText.cs
+++ StarSky/Assets/Scripts/StartText.cs
@@ -5,20 +5,20 @@ using UnityEngine.UI;
 
 public class StartText : MonoBehaviour
 {
-    float speed = 0.7f;
+    //float speed = 0.7f;
 
-    private Text text;
-    private float time;
+    //private Text text;
+    //private float time;
 
     void Start()
     {
-        text = gameObject.GetComponent<Text>();
+        //text = gameObject.GetComponent<Text>();
     }
 
     void Update()
     {
         // オブジェクトのAlpa値更新
-        text.color = GetAlphaColor(text.color);
+        //text.color = GetAlphaColor(text.color);
 
         if (Input.GetMouseButtonDown(0))
         {
@@ -27,12 +27,12 @@ public class StartText : MonoBehaviour
     }
 
     // Alpa値を更新してColorを返す
-    Color GetAlphaColor(Color color)
+    /*Color GetAlphaColor(Color color)
     {
         time += Time.deltaTime * 5.0f * speed;
         color.a = Mathf.Sin(time) * 0.9f + 0.9f;
 
         return color;
-    }
+    }*/
 
 }
